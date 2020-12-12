f4c38cdb3eb3737463a87edd466c0deb1ccdbba6
90lantran <lantran@terpmail.umd.edu>
2020-12-12 11:33:47 -0500

    integrated with go-github

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/90lantran/github-star
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/gorilla/mux v1.8.0
+)
--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/google/go-github/github"
 	"github.com/gorilla/mux"
 )
 
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
+func getStars(w http.ResponseWriter, r *http.Request) {
+	client := github.NewClient(nil)
+
+	// list all organizations for user "willnorris"
+	orgs, _, err := client.Organizations.List(context.Background(), "willnorris", nil)
+	if err != nil {
+		fmt.Printf("Cannot call go-github %v", err)
+	}
+	for i, organization := range orgs {
+		fmt.Printf("%v. %v\n", i+1, organization.GetLogin())
+	}
+
+	fmt.Fprintf(w, "Done")
 }
 
 func main() {
+	fmt.Println("... start server")
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/getStars", getStars)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
