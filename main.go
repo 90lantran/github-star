87ef8b7fd8e2e452f15fa96083bdc1001641a65a
90lantran <lantran@terpmail.umd.edu>
2020-12-12 16:04:35 -0500

    hardcode value for org and repo

--- main.go
+++ main.go
@@ -8,19 +8,51 @@ import (
 
 	"github.com/google/go-github/github"
 	"github.com/gorilla/mux"
+	"golang.org/x/oauth2"
 )
 
 func getStars(w http.ResponseWriter, r *http.Request) {
-	client := github.NewClient(nil)
+	// b082d2cd0e5e4202f31a
+	// 76b2b47503154546fc0393b70c3b488d3f6d66a1
 
-	// list all organizations for user "willnorris"
-	orgs, _, err := client.Organizations.List(context.Background(), "willnorris", nil)
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "08dcd33b87acba14d8630efbf2ae2736d885ad53"},
+	)
+	tc := oauth2.NewClient(ctx, ts)
 
-	if err != nil {
-		fmt.Printf("Cannot call go-github %v", err)
+	client := github.NewClient(tc)
+
+	opt := &github.RepositoryListByOrgOptions{
+		ListOptions: github.ListOptions{PerPage: 30},
 	}
-	for i, organization := range orgs {
-		fmt.Printf("%v. %v\n", i+1, organization.GetLogin())
+	// get all pages of results
+	var allRepos []*github.Repository
+	for {
+		repos, resp, err := client.Repositories.ListByOrg(ctx, "tinygo-org", opt)
+		if err != nil {
+			fmt.Printf("The organization does not exist %v", err)
+			return
+		}
+		allRepos = append(allRepos, repos...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+
+	// client.Repositories
+	// list all organizations for user "willnorris"
+	// repos, _, err := client.Repositories.List(context.Background(), "", nil)
+
+	// if err != nil {
+	// 	fmt.Printf("Cannot call go-github %v", err)
+	// }
+	for _, repo := range allRepos {
+		if repo.GetName() == "llvm-project" {
+			fmt.Printf("Number of star(s): %d\n", repo.GetStargazersCount())
+		}
+
 	}
 
 	fmt.Fprintf(w, "Done")
