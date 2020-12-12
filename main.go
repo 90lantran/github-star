2c76155b9c71d8d6d71f6a6519fbf9b296562638
90lantran <lantran@terpmail.umd.edu>
2020-12-12 17:44:34 -0500

    implement POST request

--- main.go
+++ main.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/google/go-github/github"
 	"github.com/gorilla/mux"
 	"golang.org/x/oauth2"
 )
 
+type request struct {
+	Input *[]string `json:"input"`
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
 func getStars(w http.ResponseWriter, r *http.Request) {
 	// b082d2cd0e5e4202f31a
 	// 76b2b47503154546fc0393b70c3b488d3f6d66a1
 
+	// should be in app.go
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: "08dcd33b87acba14d8630efbf2ae2736d885ad53"},
@@ -26,13 +45,56 @@ func getStars(w http.ResponseWriter, r *http.Request) {
 	opt := &github.RepositoryListByOrgOptions{
 		ListOptions: github.ListOptions{PerPage: 30},
 	}
+
+	// parse payload
+	var p request
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+	log.Printf("request: %v", *p.Input)
+	count := 0
+	for _, input := range *p.Input {
+		fmt.Printf("Processing input %v\n", input)
+		token := strings.Split(input, "/")
+		count += getStarsForEachRepo(token[0], token[1], client, ctx, opt)
+	}
 	// get all pages of results
+	// var count int
+	// var allRepos []*github.Repository
+	// for {
+	// 	repos, resp, err := client.Repositories.ListByOrg(ctx, "tinygo-org", opt)
+	// 	if err != nil {
+	// 		fmt.Printf("The organization does not exist %v", err)
+	// 		return
+	// 	}
+	// 	allRepos = append(allRepos, repos...)
+	// 	if resp.NextPage == 0 {
+	// 		break
+	// 	}
+	// 	opt.Page = resp.NextPage
+	// }
+
+	// for _, repo := range allRepos {
+	// 	if repo.GetName() == "llvm-project" {
+	// 		count += repo.GetStargazersCount()
+	// 		//fmt.Printf("Number of star(s): %d\n", repo.GetStargazersCount())
+	// 	}
+	// }
+	log.Println(count)
+	respondWithJSON(w, http.StatusOK, count)
+}
+
+func getStarsForEachRepo(orgName string, repoName string, client *github.Client, ctx context.Context, opt *github.RepositoryListByOrgOptions) int {
+	var count int
 	var allRepos []*github.Repository
 	for {
-		repos, resp, err := client.Repositories.ListByOrg(ctx, "tinygo-org", opt)
+		repos, resp, err := client.Repositories.ListByOrg(ctx, orgName, opt)
 		if err != nil {
 			fmt.Printf("The organization does not exist %v", err)
-			return
+			continue
 		}
 		allRepos = append(allRepos, repos...)
 		if resp.NextPage == 0 {
@@ -41,26 +103,19 @@ func getStars(w http.ResponseWriter, r *http.Request) {
 		opt.Page = resp.NextPage
 	}
 
-	// client.Repositories
-	// list all organizations for user "willnorris"
-	// repos, _, err := client.Repositories.List(context.Background(), "", nil)
-
-	// if err != nil {
-	// 	fmt.Printf("Cannot call go-github %v", err)
-	// }
 	for _, repo := range allRepos {
-		if repo.GetName() == "llvm-project" {
-			fmt.Printf("Number of star(s): %d\n", repo.GetStargazersCount())
+		if repo.GetName() == repoName {
+			count += repo.GetStargazersCount()
+			fmt.Printf("Found: %s\n", repoName)
 		}
-
 	}
-
-	fmt.Fprintf(w, "Done")
+	return count
 }
 
 func main() {
 	fmt.Println("... start server")
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/getStars", getStars)
+	router.HandleFunc("/get-stars", getStars).Methods("POST")
+	//router.HandleFunc("/getStars", getStars)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
---
+++ model.go
@@ -0,0 +1,5 @@
+package main
+
+type request struct {
+	Input *[]string `json:"input"`
+}
