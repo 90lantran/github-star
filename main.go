21554c81ec1af221fee3bf7541b41b5d7b97e07c
90lantran <lantran@terpmail.umd.edu>
2020-12-13 18:07:29 -0500

    design payload and refactor

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,6 +16,12 @@ import (
 
 type request struct {
 	Input *[]string `json:"input"`
+}
+
+type response struct {
+	TotalStars   int            `json:"totalStars,omitempty"`
+	InvalidRepos []string       `json:"invalidRepos,omitempty"`
+	ValidRepos   map[string]int `json:"validRepos,omitempty"`
 }
 
 func respondWithError(w http.ResponseWriter, code int, message string) {
@@ -55,20 +62,80 @@ func getStars(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 	log.Printf("request: %v", *p.Input)
-	count := 0
+
+	totalCount := 0
+	validRepos := make(map[string]int)
+	invalidRepos := make([]string, 0)
+	var resp response
+	seenOrgs := make(map[string][]*github.Repository)
 	for _, input := range *p.Input {
 		fmt.Printf("Processing input %v\n", input)
 		token := strings.Split(input, "/")
-		count += getStarsForEachRepo(token[0], token[1], client, ctx, opt)
+		allRepos, ok := seenOrgs[token[0]]
+		if !ok {
+			fmt.Printf("%s is not in the map\n", token[0])
+			results, err := listAllReposForAnOrg(ctx, token[0], client, opt)
+			if err == nil {
+				fmt.Printf("Find all repos for %s\n", token[0])
+				allRepos = results
+				seenOrgs[token[0]] = allRepos
+
+			} else {
+				// invalid org
+				fmt.Printf("%s is not a valid org\n", token[0])
+				invalidRepos = append(invalidRepos, input)
+				continue
+			}
+
+		}
+
+		// for i, r := range allRepos {
+		// 	fmt.Printf("%d. %v\n", i+1, r.GetName())
+		// }
+
+		if count := getStarsForRepo(allRepos, token[1]); count != -1 {
+			validRepos[input] = count
+			totalCount += count
+		} else {
+			// invalid repo
+			fmt.Printf("%s is not a valid repo\n", token[1])
+			invalidRepos = append(invalidRepos, input)
+		}
+
 	}
-	// get all pages of results
-	// var count int
+
+	resp.TotalStars = totalCount
+	resp.ValidRepos = validRepos
+	resp.InvalidRepos = invalidRepos
+	respondWithJSON(w, http.StatusOK, resp)
+}
+
+func listAllReposForAnOrg(ctx context.Context, orgName string, client *github.Client, opt *github.RepositoryListByOrgOptions) ([]*github.Repository, error) {
+	var allRepos []*github.Repository
+	for {
+		repos, resp, err := client.Repositories.ListByOrg(ctx, orgName, opt)
+		if err != nil {
+			fmt.Printf("the organization %s does not exist %v", orgName, err)
+			return nil, errors.New("the organization does not exist")
+		}
+		allRepos = append(allRepos, repos...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+	return allRepos, nil
+
+}
+
+func getStarsForRepo(allRepos []*github.Repository, repoName string) int {
+	var count int
 	// var allRepos []*github.Repository
 	// for {
-	// 	repos, resp, err := client.Repositories.ListByOrg(ctx, "tinygo-org", opt)
+	// 	repos, resp, err := client.Repositories.ListByOrg(ctx, orgName, opt)
 	// 	if err != nil {
 	// 		fmt.Printf("The organization does not exist %v", err)
-	// 		return
+	// 		return -1
 	// 	}
 	// 	allRepos = append(allRepos, repos...)
 	// 	if resp.NextPage == 0 {
@@ -77,39 +144,14 @@ func getStars(w http.ResponseWriter, r *http.Request) {
 	// 	opt.Page = resp.NextPage
 	// }
 
-	// for _, repo := range allRepos {
-	// 	if repo.GetName() == "llvm-project" {
-	// 		count += repo.GetStargazersCount()
-	// 		//fmt.Printf("Number of star(s): %d\n", repo.GetStargazersCount())
-	// 	}
-	// }
-	log.Println(count)
-	respondWithJSON(w, http.StatusOK, count)
-}
-
-func getStarsForEachRepo(orgName string, repoName string, client *github.Client, ctx context.Context, opt *github.RepositoryListByOrgOptions) int {
-	var count int
-	var allRepos []*github.Repository
-	for {
-		repos, resp, err := client.Repositories.ListByOrg(ctx, orgName, opt)
-		if err != nil {
-			fmt.Printf("The organization does not exist %v", err)
-			continue
-		}
-		allRepos = append(allRepos, repos...)
-		if resp.NextPage == 0 {
-			break
-		}
-		opt.Page = resp.NextPage
-	}
-
 	for _, repo := range allRepos {
 		if repo.GetName() == repoName {
 			count += repo.GetStargazersCount()
 			fmt.Printf("Found: %s\n", repoName)
+			return count
 		}
 	}
-	return count
+	return -1
 }
 
 func main() {
